bd436b04623e0514ceeb5d50d9b33390c9d33515
Mario Contreras <mariocc29@gmail.com>
2024-05-13 16:30:00 -0400

    enhanced inventory data to client

--- app/jobs/web_socket_job.rb
+++ app/jobs/web_socket_job.rb
@@ -5,17 +5,27 @@ class WebSocketJob < ApplicationJob
   queue_as :default
 
   def perform(message)
+    inventory = persists(message)
+    build(inventory) do | data |
+      ActionCable.server.broadcast('NotificationChannel', { type: NotificationCategory::TRANSACTION, data: })
+    end
+  rescue StandardError => e
+    Rails.logger.error "WebSocketJob error: #{e.message}"
+  end
+
+  private
+
+  def persists(message)
     inventory = Inventory.new
     inventory.store = Store.find_or_create_by(name: message['store'])
     inventory.model = Model.find_or_create_by(name: message['model'])
     inventory.stock = message['inventory']
+    inventory.save
 
-    begin
-      inventory.save
-      inventory.includes(:store, :model)
-      ActionCable.server.broadcast('NotificationChannel', { type: NotificationCategory::TRANSACTION, data: inventory })
-    rescue StandardError => e
-      Rails.logger.error "WebSocketJob error: #{e.message}"
-    end
+    inventory
+  end
+
+  def build(inventory)
+    yield( inventory.attributes.merge(store: inventory.store, model: inventory.model) )
   end
 end
