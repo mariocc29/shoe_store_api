3c6d1549e838e901112589720abb8ca14f390dec
Mario Contreras <mariocc29@gmail.com>
2024-05-15 17:59:35 -0400

    hotfix inventory handler

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,8 +3,19 @@ class Inventory < ApplicationRecord
 
   belongs_to :store
   belongs_to :model
-
+  
   after_create :send_to_sqs
+  
+  scope :latest_stock, -> do
+    subquery = group(:store_id, :model_id).select("store_id, model_id, MAX(created_at) as max_created_at")
+    where("(store_id, model_id, inventories.created_at) IN (?)", subquery)
+  end
+  
+  scope :last_year_stock, -> do
+    last_year = (Date.today - 1.year).beginning_of_year..(Date.today - 1.year).end_of_year
+    subquery = group(:store_id, :model_id).select("store_id, model_id, MAX(created_at) as max_created_at").where({ created_at: last_year })
+    where("(store_id, model_id, inventories.created_at) IN (?)", subquery)
+  end
 
   private
 
--- app/services/inventories/process.rb
+++ app/services/inventories/process.rb
@@ -9,37 +9,25 @@ module Inventories
       {
         total: current_inventory[:total],
         inventory_growth: inventory_growth(current_inventory[:total], last_year_inventory),
-        current_inventory: current_inventory[:stores]
+        current_inventory: current_inventory[:inventory]
       }
     end
 
     private
 
     def get_current_inventory_data
-      stores = Store.joins(:models)
-                    .select('stores.id AS store_id, stores.name AS store_name, models.id AS model_id, models.name AS model_name, MAX(inventories.stock) AS last_stock')
-                    .joins('LEFT JOIN inventories ON inventories.store_id = stores.id AND inventories.model_id = models.id')
-                    .group('stores.id, models.id')
+      inventory = Inventory.select('inventories.model_id, inventories.store_id, inventories.stock, stores.name AS store_name, models.name')
+                  .joins(:model)
+                  .joins(:store)
+                  .latest_stock
 
-      total = 0
-      stores.each { |store| total += store.last_stock }
-
-      { total:, stores: }
+      total = inventory.sum(:stock)
+      
+      { inventory:, total: }
     end
 
     def get_last_year_inventory_data
-      last_year = (Date.today - 1.year).beginning_of_year..(Date.today - 1.year).end_of_year
-
-      stores = Store.joins(:models)
-                    .select('stores.name AS store_name, models.name AS model_name, MAX(inventories.stock) AS last_stock')
-                    .joins('LEFT JOIN inventories ON inventories.store_id = stores.id AND inventories.model_id = models.id')
-                    .where(inventories: { created_at: last_year })
-                    .group('stores.id, models.id')
-
-      total = 0
-      stores.each { |store| total += store.last_stock }
-
-      total
+      Inventory.last_year_stock.sum(:stock)
     end
 
     def inventory_growth(current_inventory, last_year_inventory)
